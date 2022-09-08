014a7a09d53af145dd9f0bce784934006331497c
anhduc <ducank961@gmail.com>
2022-09-08 14:52:02 +0700

    Tigren_Question
    Signed-off-by: anhduc <ducank961@gmail.com>

---
+++ app/code/Tigren/Question/Block/Create.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Tigren\Question\Block;
+use Magento\Framework\View\Element\Template;
+
+class Create extends Template
+{
+
+}
