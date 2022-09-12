3c000ea9e7a23c60aab195bb2729ae0fca361baf
anhduc <ducank961@gmail.com>
2022-09-12 14:31:25 +0700

    Tigren_CustomerGroupCatalog
    Signed-off-by: anhduc <ducank961@gmail.com>

---
+++ app/code/Tigren/CustomerGroupCatalog/Controller/RegistryConstants.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Tigren\CustomerGroupCatalog\Controller;
+
+class RegistryConstants
+{
+    /**
+     * Key for current groupcat rule in registry
+     */
+    public const CURRENT_GROUPCATALOG_RULE_ID = 'current_tigren_customer_groupcatalog_rule';
+}
---
+++ app/code/Tigren/CustomerGroupCatalog/Plugin/Magento/Catalog/Model/Product/Hidecartbutton.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Tigren\CustomerGroupCatalog\Plugin\Magento\Catalog\Model\Product;
+
+use Magento\Catalog\Model\Product;
+use Magento\Customer\Model\Context;
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Http\Context as AuthContext;
+
+/**
+ * Class Hidecartbutton
+ *
+ * @package Tigren\CustomerGroupCatalog\Plugin
+ */
+class Hidecartbutton
+{
+    protected $customerSession;
+    protected $authContext;
+
+    public function __construct(Session $session, AuthContext $authContext)
+    {
+        $this->customerSession = $session;
+        $this->authContext = $authContext;
+    }
+
+    /**
+     * @param  Product  $product
+     *
+     * @return array
+     */
+    public function afterIsSaleable(Product $product, $result)
+    {
+        // using HTTP context
+        if ($this->authContext->getValue(Context::CONTEXT_AUTH)) {
+            return $result;
+        } else {
+            return [];
+        }
+
+    }
+}
