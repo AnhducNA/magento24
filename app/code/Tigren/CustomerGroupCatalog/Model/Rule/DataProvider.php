81edd47390fce17a2201e54f072c741d1c59d804
anhduc <ducank961@gmail.com>
2022-09-13 09:44:29 +0700

    Tigren_CustomerGroupCatalog
    Signed-off-by: anhduc <ducank961@gmail.com>

--- app/code/Tigren/CustomerGroupCatalog/Model/Rule/DataProvider.php
+++ app/code/Tigren/CustomerGroupCatalog/Model/Rule/DataProvider.php
@@ -36,9 +36,9 @@ class DataProvider extends AbstractDataProvider
     protected $dataPersistor;
 
     /**
-     * @param $name
-     * @param $primaryFieldName
-     * @param $requestFieldName
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
      * @param CollectionFactory $collectionFactory
      * @param DataPersistorInterface $dataPersistor
      * @param array $meta
