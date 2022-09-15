b31fe3ff148e02ed7b2302fb87dbf8ed3066ac17
anhduc <ducank961@gmail.com>
2022-09-15 16:32:14 +0700

    Tigren_AdvancedCheckout

--- app/code/Tigren/AdvancedCheckout/Override/Block/CatalogWidget/Product/ProductsList.php
+++ app/code/Tigren/AdvancedCheckout/Override/Block/CatalogWidget/Product/ProductsList.php
@@ -1,9 +1,4 @@
 <?php
-/*
- * @author    Tigren Solutions <info@tigren.com>
- * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
- * @license   Open Software License ("OSL") v. 3.0
- */
 
 namespace Tigren\AdvancedCheckout\Override\Block\CatalogWidget\Product;
 
@@ -13,5 +8,4 @@ class ProductsList extends \Magento\CatalogWidget\Block\Product\ProductsList
     {
         $this->setTemplate('Tigren_AdvancedCheckout::product/widget/content/grid.phtml');
     }
-
 }
--- app/code/Tigren/AdvancedCheckout/view/frontend/web/js/catalog-add-to-cart.js
+++ app/code/Tigren/AdvancedCheckout/view/frontend/web/js/catalog-add-to-cart.js
@@ -1,32 +1,32 @@
-define(["jquery", "domReady!", "Magento_Ui/js/modal/modal"], function ($, dom) {
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
 
-    // $(document).ready(function(){
-    //     $(".tocart").click(function (){
-    //         alert("Add Cataloggg Js");
-    //     });
-    // })
+define(["jquery", "domReady!", "Magento_Ui/js/modal/modal"], function ($, dom) {
 
     "use strict";
     //creating jquery widget
     $.widget('Vendor.modalForm', {
         options: {
             modalForm: '#modal-form',
-            modalButton: '.open-modal-form'
+            modalButton: '.tocart'
         },
-        _create: function() {
+        _create: function () {
             this.options.modalOption = this._getModalOptions();
             this._bind();
         },
-        _getModalOptions: function() {
+        _getModalOptions: function () {
             /**
              * Modal options
              */
             var options = {
                 type: 'popup',
                 responsive: true,
-                title: 'Sample Title',
+                title: 'Allow multi products ',
                 buttons: [{
-                    text: $.mage.__('Continue'),
+                    text: $.mage.__('Close model'),
                     class: '',
                     click: function () {
                         this.closeModal();
@@ -36,15 +36,38 @@ define(["jquery", "domReady!", "Magento_Ui/js/modal/modal"], function ($, dom) {
 
             return options;
         },
-        _bind: function(){
+        _bind: function () {
             var modalOption = this.options.modalOption;
             var modalForm = this.options.modalForm;
 
-            $(document).on('click', this.options.modalButton,  function(){
-                //Initialize modal
-                $(modalForm).modal(modalOption);
-                //open modal
-                $(modalForm).trigger('openModal');
+            $(document).on('click', this.options.modalButton, function () {
+
+
+                var data_product_sku = $(this).attr('data-product-sku');
+                console.log(data_product_sku);
+                // var popupUrl = "<?php echo $this->getUrl().'tigren_advancedcheckout/index/showpopup'; ?>";
+                // var popupUrl = "http://magento24.localhost.com/tigren_advancedcheckout/index/showpopup";
+                var popupUrl = window.location.origin+"/tigren_advancedcheckout/index/showpopup";
+                console.log(popupUrl);
+                //callajax
+                // debugger;
+                $.ajax({
+                    url: popupUrl,
+                    data: {
+                        sku: data_product_sku
+                    },
+                    type: "POST",
+                    dataType: 'json'
+                }).done(function (response) {
+                    console.log(1);
+                    if (response.isShowPopup) {
+                        //Initialize modal
+                        $(modalForm).modal(modalOption);
+                        //open modal
+                        $(modalForm).trigger('openModal');
+                    }
+                });
+
             });
         }
     });
