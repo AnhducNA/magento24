54939c1896ae209d60f5b408ff005c588dfd8c26
anhduc <ducank961@gmail.com>
2022-09-20 15:21:24 +0700

    Tigren_Customer

---
+++ app/code/Tigren/Customer/Block/Frontend/Index.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Tigren\Customer\Block\Frontend;
+
+use Magento\Customer\Model\Session;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Tigren\Customer\Model\QuestionFactory;
+use Tigren\Customer\Model\ResourceModel\Question\CollectionFactory;
+
+/**
+ * Class Index
+ * @package Tigren\Question\Block
+ */
+class Index extends Template
+{
+    /**
+     * @var QuestionFactory
+     */
+    protected $_questionFactory;
+    /**
+     * @var Session
+     */
+    protected $_customerSession;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $_collectionFactory;
+
+    /**
+     * @param Context $context
+     * @param QuestionFactory $questionFactory
+     * @param Session $customerSession
+     * @param array $data
+     */
+    public function __construct(
+        Context           $context,
+        QuestionFactory   $questionFactory,
+        Session           $customerSession,
+        CollectionFactory $collectionFactory,
+        array             $data = []
+    ) {
+        $this->_customerSession = $customerSession;
+        $this->_questionFactory = $questionFactory;
+        $this->_collectionFactory = $collectionFactory;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return array
+     */
+    public function getCollection()
+    {
+        if ($this->_customerSession->isLoggedIn()) {
+            $idCustomerLogin = $this->_customerSession->getCustomerId();
+//            echo $idCustomerLogin ? $idCustomerLogin : null;
+            if ($idCustomerLogin) {
+                $collection = $this->_collectionFactory->create();
+                foreach ($collection as $key=> $value) {
+                    if ($value->getAuthorId() == $idCustomerLogin) {
+                        $collectionByID[$key] = $value;
+                    }
+                }
+            }
+        }
+
+        return $collectionByID;
+    }
+}
---
+++ app/code/Tigren/Customer/Model/Question/DataProvider.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Tigren\Customer\Model\Question;
+
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Tigren\Customer\Model\Question;
+use Tigren\Customer\Model\ResourceModel\Question\Collection;
+use Tigren\Customer\Model\ResourceModel\Question\CollectionFactory;
+
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * @var Collection
+     */
+    protected $collection;
+
+    /**
+     * @var array
+     */
+    protected $loadedData;
+
+    /**
+     * @var DataPersistorInterface
+     */
+    protected $dataPersistor;
+
+    /**
+     * @param $name
+     * @param $primaryFieldName
+     * @param $requestFieldName
+     * @param CollectionFactory $collectionFactory
+     * @param DataPersistorInterface $dataPersistor
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        DataPersistorInterface $dataPersistor,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->collection = $collectionFactory->create();
+        $this->dataPersistor = $dataPersistor;
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
+
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+        $items = $this->collection->getItems();
+        /** @var Question $question */
+        foreach ($items as $question) {
+            $question->load($question->getId());
+            $this->loadedData[$question->getId()] = $question->getData();
+        }
+
+        $data = $this->dataPersistor->get('tigren_customer_question');
+        if (!empty($data)) {
+            $question = $this->collection->getNewEmptyItem();
+            $question->setData($data);
+            $this->loadedData[$question->getId()] = $question->getData();
+            $this->dataPersistor->clear('tigren_customer_question');
+        }
+
+        return $this->loadedData;
+    }
+}
